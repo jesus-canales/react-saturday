993f5399ccc95531202f561c65ef4d315547ce3c
jesus-canales <177686251+jesus-canales@users.noreply.github.com>
2024-08-17 23:33:08 +0000

    He agregado un componente con parámetros y ademas reutilizable

--- AppReact/src/App.jsx
+++ AppReact/src/App.jsx
@@ -2,6 +2,7 @@ import { Fragment } from 'react';
 import Mensaje from './componente/Mensaje'
 import Encabezado from './componente/Encabezado'
 import Pie from './componente/Pie'
+import Ciudad from './componente/Ciudad'
 
 
 function App() {
@@ -9,6 +10,16 @@ function App() {
   return (
     <>
       <Encabezado />
+      <Ciudad
+      nombre = "Lima"
+      descripcion = "Capital del Perú"
+      imagen = 'https://www.xtravelperu.com/wp-content/uploads/2017/09/lima-miraflores-x-travel-peru.jpg'
+       />
+      <Ciudad
+      nombre = "Arequipa"
+      descripcion= "Encantadora tierra volcánica"
+      imagen = "https://www.peru.travel/Contenido/Destino/Imagen/es/3/1.2/Principal/Plaza%20de%20Armas%20Arequipa.jpg" />
+
       <Pie />
     </>
   );

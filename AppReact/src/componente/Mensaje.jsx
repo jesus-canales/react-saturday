6bad66ba4d11c65111f3f0581c9acb99e903c457
jesus-canales <177686251+jesus-canales@users.noreply.github.com>
2024-08-17 22:55:04 +0000

    He creado un componente llamado Mensaje y lo he llamado desde el componente App.jsx

---
+++ AppReact/src/componente/Mensaje.jsx
@@ -0,0 +1,13 @@
+import { Fragment } from "react";
+
+function Mensaje(){
+
+    return (
+        <>
+        <h1>Jesús Canales Guando</h1>
+        <h3>Docente de DEW-2024</h3>
+        </>
+    );
+}
+
+export default Mensaje;

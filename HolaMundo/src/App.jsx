aeb923b94c544d634ed3786c0a6e410f235d73cf
jesus-canales <177686251+jesus-canales@users.noreply.github.com>
2024-08-17 21:46:42 +0000

    He ejecutado mi app con react y además hemos validado el stock de lo productos

--- HolaMundo/src/App.jsx
+++ HolaMundo/src/App.jsx
@@ -1,7 +1,9 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import ciberteclogo from './image/cibertec-logo.png'
 import './App.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -15,8 +17,11 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={ciberteclogo} className="logo" alt="Vite logo" />
+        </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React + Cibertec</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}

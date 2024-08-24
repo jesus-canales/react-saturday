ffa444d125957dcf9737a96391e8a2ef8295e780
jesus-canales <177686251+jesus-canales@users.noreply.github.com>
2024-08-24 21:39:19 +0000

    He creado un componente Mensaje

---
+++ AppDemo/src/componente/Mensaje.jsx
@@ -0,0 +1,12 @@
+import { Fragment } from "react"
+
+function Mensaje(){
+    return (
+        <>
+        <h1>Laboratorio de Ejercicios Práctico</h1>
+        <h4>Estudiante: Jesús Canales</h4>
+        </>
+    )
+}
+
+export default Mensaje;
